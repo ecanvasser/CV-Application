fee27ff5bc5e264661f05a86bf3120cd77131b0a
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-20 23:42:00 -0800

    Add and link state object to name input

--- src/components/Document.js
+++ src/components/Document.js
@@ -2,11 +2,24 @@ import { Component } from "react";
 import "../styles/Document.css";
 
 class Document extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({name: e.target.value})
+  }
+  
+    render() {
     return (
       <div id="document">
         <form id="name">
-          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Name" onChange={this.handleChange}/>
         </form>
         <div id="name-divider"></div>
       </div>
