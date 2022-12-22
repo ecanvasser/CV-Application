8cbdc779d9dceecae688d8058e78ac677ec1fc15
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-21 22:29:25 -0800

    Change contact details layout

--- src/components/Document.js
+++ src/components/Document.js
@@ -9,7 +9,12 @@ class Document extends Component {
       name: "",
       email: "",
       phone: "",
-      location: ""
+      education: {
+        location: "",
+        school: "",
+        degree: "",
+        duration: "",
+      },
     };
 
     this.nameChange = this.nameChange.bind(this);
@@ -33,25 +38,70 @@ class Document extends Component {
           </form>
           <form id="contact">
             <input
-              id="email" 
+              id="email"
               type="email"
               placeholder="Email"
-              onChange={(e) => this.setState({email: e.target.value})}
+              onChange={(e) => this.setState({ email: e.target.value })}
               value={this.state.email}
             />
             <input
               id="phone"
               type="phone"
               placeholder="Phone"
-              onChange={(e) => this.setState({phone: e.target.value})}
+              onChange={(e) => this.setState({ phone: e.target.value })}
               value={this.state.phone}
             />
             <input
               id="location"
               type="text"
               placeholder="City, State"
-              onChange={(e) => this.setState({location: e.target.value})}
+              onChange={(e) => this.setState({ location: e.target.value })}
               value={this.state.location}
+            />
+          </form>
+          <div id="edu-title">Education</div>
+          <form id="education">
+            <input
+              id="school"
+              type="text"
+              placeholder="Organization"
+              onChange={(e) =>
+                this.setState({
+                  education: {
+                    ...this.state.education,
+                    school: e.target.value,
+                  },
+                })
+              }
+              value={this.state.education.school}
+            />
+            <input
+              id="degree"
+              type="text"
+              placeholder="Degree/Major"
+              onChange={(e) =>
+                this.setState({
+                  education: {
+                    ...this.state.education,
+                    degree: e.target.value,
+                  },
+                })
+              }
+              value={this.state.education.degree}
+            />
+            <input
+              id="startend"
+              type="text"
+              placeholder="Start year - End Year"
+              onChange={(e) =>
+                this.setState({
+                  education: {
+                    ...this.state.education,
+                    duration: e.target.value,
+                  },
+                })
+              }
+              value={this.state.education.duration}
             />
           </form>
         </div>
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -10,12 +10,19 @@ class Preview extends Component {
     return (
     <div id="preview">
       <div id="name-prev">{this.props.data.name}</div>
+      <div id="name-divider"></div>
       <div id="contact-prev">
         <div id="email">{this.props.data.email}</div>
         <div id="phone">{this.props.data.phone}</div>
         <div id="location">{this.props.data.location}</div>
       </div>
       <div id="name-divider"></div>
+      <div id="edu-title">Education</div>
+      <div id="edu-details">
+        <div id="school">{this.props.data.education.school}</div>
+        <div id="degree">{this.props.data.education.degree}</div>
+        <div id="startend">{this.props.data.education.duration}</div>
+      </div>
     </div>);
   }
 }
