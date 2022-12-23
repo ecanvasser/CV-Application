ab872b3a765daa4137a828d8572db1a64789c4c1
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-22 21:38:27 -0800

    Add and render EducationForm component inside Document.js

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Preview from "./Preview";
 import GeneralForm from "./GeneralForm";
+import EducationForm from "./EducationForm";
 import "../styles/Document.css";
 
 class Document extends Component {
@@ -19,71 +20,43 @@ class Document extends Component {
       },
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleGeneralChange = this.handleGeneralChange.bind(this);
+    this.handleEducationChange = this.handleEducationChange.bind(this);
   }
 
-  handleChange(e) {
+  handleGeneralChange(e) {
     const value = e.target.value;
     this.setState({ ...this.state, [e.target.name]: value });
+  }
+
+  handleEducationChange(e) {
+    const value = e.target.value;
+    this.setState({
+      education: {
+        ...this.state.education,
+        [e.target.name]: value,
+      },
+    });
   }
 
   render() {
     if (this.props.preview === false) {
       return (
         <div id="document">
-          <GeneralForm 
-            handleChange={this.handleChange}
+          <GeneralForm
+            handleChange={this.handleGeneralChange}
             name={this.state.name}
             role={this.state.role}
             email={this.state.email}
             phone={this.state.phone}
             location={this.state.location}
           />
-          <div id="edu-title-edit">Education</div>
-          <form id="education">
-            <input
-              id="school"
-              type="text"
-              placeholder="Organization"
-              onChange={(e) =>
-                this.setState({
-                  education: {
-                    ...this.state.education,
-                    school: e.target.value,
-                  },
-                })
-              }
-              value={this.state.education.school}
-            />
-            <input
-              id="degree"
-              type="text"
-              placeholder="Degree/Major"
-              onChange={(e) =>
-                this.setState({
-                  education: {
-                    ...this.state.education,
-                    degree: e.target.value,
-                  },
-                })
-              }
-              value={this.state.education.degree}
-            />
-            <input
-              id="startend"
-              type="text"
-              placeholder="Start year - End Year"
-              onChange={(e) =>
-                this.setState({
-                  education: {
-                    ...this.state.education,
-                    duration: e.target.value,
-                  },
-                })
-              }
-              value={this.state.education.duration}
-            />
-          </form>
+          <EducationForm
+            handleChange={this.handleEducationChange}
+            school={this.state.education.school}
+            degree={this.state.education.degree}
+            duration={this.state.education.duration}
+          />
         </div>
       );
     }
---
+++ src/components/EducationForm.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+class EducationForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <div id="edu-title-edit">Education</div>
+        <form id="education">
+          <input
+            id="school"
+            name="school"
+            type="text"
+            placeholder="Organization"
+            onChange={this.props.handleChange}
+            value={this.props.school}
+          />
+          <input
+            id="degree"
+            name="degree"
+            type="text"
+            placeholder="Degree/Major"
+            onChange={this.props.handleChange}
+            value={this.props.degree}
+          />
+          <input
+            id="startend"
+            name="duration"
+            type="text"
+            placeholder="Start year - End Year"
+            onChange={this.props.handleChange}
+            value={this.props.duration}
+          />
+        </form>
+      </>
+    );
+  }
+}
+
+export default EducationForm;
