6dff2bd7cf801665d2e2ed7ca4b9513c66eaa7b5
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-28 22:34:15 -0800

    Add dynamic inputs for job tasks

--- src/components/ExperienceForm.js
+++ src/components/ExperienceForm.js
@@ -1,17 +1,43 @@
 import { Component } from "react";
-import '../styles/ExperienceForm.css'
+import "../styles/ExperienceForm.css";
 
 class ExperienceForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobSummCount: 1,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = () => {
+    let count = this.state.jobSummCount;
+
+    if (count < 3) {
+      this.setState({ ...this.state, ["jobSummCount"]: count + 1 });
+    }
+  };
+
   render() {
     return (
       <>
         <div id="exp-form">
-          <form>
+          <form
+            id="form"
+            onSubmit={(e) => {
+              this.handleSubmit();
+              e.preventDefault();
+            }}
+          >
             <input placeholder="Company" />
             <input placeholder="Job Title" />
             <input placeholder="Location" />
-            <input placeholder="Start & End Date"/>
-            <input placeholder="Job Summary" />
+            <input placeholder="Start & End Date" />
+            {[...Array(this.state.jobSummCount)].map((e) => {
+                return <input placeholder="Job Summary" />
+            })}
+            <input type="submit" hidden />
           </form>
         </div>
       </>
