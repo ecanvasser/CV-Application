786a592f38ad0a2fec09ebaf3785cf3681f582c3
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-01 23:07:30 -0800

    Initialize ExperiencePreview.js and ExperiencePreview.css

---
+++ src/components/ExperiencePreview.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import '../styles/ExperiencePreview.css';
+
+class ExperiencePreview extends Component {
+    constructor(props) {
+        super(props)
+    }
+}
+
+export default ExperiencePreview
