1590a430ea14fd8e10000b8c153bc4695536194b
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 14:28:26 -0800

    Convert Content.js to function based component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,12 @@
-import { Component } from "react";
 import Document from "./Document";
 import "../styles/Content.css";
 
-class Content extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <main>
-        <Document preview={this.props.preview} />
-      </main>
-    );
-  }
+const Content = (props) => {
+  return (
+    <main>
+      <Document preview={props.preview} />
+    </main>
+  );
 }
 
 export default Content;
