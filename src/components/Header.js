4724ebba3c5edfe73d6762c103371b1ed01d276b
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 13:39:18 -0800

    Convert Header.js to function based component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,19 @@
-import { Component } from "react";
 import "../styles/Header.css";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <header>
-        <div id="header-title">CV App</div>
-        <div id="header-links">
-          <button onClick={() => this.props.changeEdit()} id="edit-btn">
-            Edit
-          </button>
-          <button onClick={() => this.props.changePreview()} id="prev-btn">
-            Preview
-          </button>
-        </div>
-      </header>
-    );
-  }
-}
+const Header = (props) => {
+  return (
+    <header>
+      <div id="header-title">CV App</div>
+      <div id="header-links">
+        <button onClick={() => props.changeEdit()} id="edit-btn">
+          Edit
+        </button>
+        <button onClick={() => props.changePreview()} id="prev-btn">
+          Preview
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
