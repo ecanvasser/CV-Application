b3da722c344533d3a0d20b36ccd31b4eea0c3249
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 14:51:12 -0800

    Convert Preview.js to function based component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import "../styles/Preview.css";
 import ExperiencePreview from "./ExperiencePreview";
 import { MdLocationOn } from "react-icons/md";
@@ -6,90 +5,76 @@ import { MdEmail } from "react-icons/md";
 import { MdSmartphone } from "react-icons/md";
 import { IconContext } from "react-icons";
 
-class Preview extends Component {
-  constructor(props) {
-    super(props);
-    this.renderSkills = this.renderSkills.bind(this);
-  }
-
-  renderSkills() {
-    let skills = this.props.data.skills;
-    skills.map((skill) => {
-      return <div className="skill-block">{skill}</div>;
-    });
-  }
-
-  render() {
-    return (
-      <div id="preview">
-        <div id="name-prev">{this.props.data.name}</div>
-        <div id="role">{this.props.data.role}</div>
-        <div id="divider"></div>
-        <div id="contact-prev">
-          <div id="email-prev">
-            <IconContext.Provider value={{ color: "#0891b2", size: "1.4em" }}>
-              <div>
-                <MdEmail />
-              </div>
-            </IconContext.Provider>
-            {this.props.data.email}
-          </div>
-          <div id="phone-prev">
+const Preview = (props) => {
+  return (
+    <div id="preview">
+      <div id="name-prev">{props.data.name}</div>
+      <div id="role">{props.data.role}</div>
+      <div id="divider"></div>
+      <div id="contact-prev">
+        <div id="email-prev">
           <IconContext.Provider value={{ color: "#0891b2", size: "1.4em" }}>
-              <div>
-                <MdSmartphone />
-              </div>
-            </IconContext.Provider>
-            {this.props.data.phone}
+            <div>
+              <MdEmail />
             </div>
-          <div id="location-prev">
-            <IconContext.Provider value={{ color: "#0891b2", size: "1.4em" }}>
-              <div>
-                <MdLocationOn />
-              </div>
-            </IconContext.Provider>
-            {this.props.data.location}
-          </div>
+          </IconContext.Provider>
+          {props.data.email}
         </div>
-        <div id="divider"></div>
-        <div id="edu-skills">
-          <div id="edu-half">
-            <div id="edu-title">Education</div>
-            <div id="edu-details">
-              <div id="school-prev">{this.props.data.education.school}</div>
-              <div id="degree-prev">{this.props.data.education.degree}</div>
-              <div id="startend-prev">{this.props.data.education.duration}</div>
+        <div id="phone-prev">
+        <IconContext.Provider value={{ color: "#0891b2", size: "1.4em" }}>
+            <div>
+              <MdSmartphone />
             </div>
+          </IconContext.Provider>
+          {props.data.phone}
           </div>
-          <div id="skill-half">
-            <div id="skills-title">Skills</div>
-            <div id="skill-list">
-              {this.props.data.skills.map((skill) => {
-                return <div className="skill-block">{skill}</div>;
-              })}
+        <div id="location-prev">
+          <IconContext.Provider value={{ color: "#0891b2", size: "1.4em" }}>
+            <div>
+              <MdLocationOn />
             </div>
-          </div>
-        </div>
-        <div id="divider"></div>
-        <div id="exp-title">Experience</div>
-        <div id="prevExp">
-          {Array.from(Object.values(this.props.data.experience.inputs)).map(
-            (e) => {
-              return (
-                <ExperiencePreview
-                  company={e["company"]}
-                  dates={e["dates"]}
-                  location={e["location"]}
-                  title={e["title"]}
-                  tasks={e["tasks"]}
-                />
-              );
-            }
-          )}
+          </IconContext.Provider>
+          {props.data.location}
         </div>
       </div>
-    );
-  }
+      <div id="divider"></div>
+      <div id="edu-skills">
+        <div id="edu-half">
+          <div id="edu-title">Education</div>
+          <div id="edu-details">
+            <div id="school-prev">{props.data.education.school}</div>
+            <div id="degree-prev">{props.data.education.degree}</div>
+            <div id="startend-prev">{props.data.education.duration}</div>
+          </div>
+        </div>
+        <div id="skill-half">
+          <div id="skills-title">Skills</div>
+          <div id="skill-list">
+            {props.data.skills.map((skill) => {
+              return <div className="skill-block">{skill}</div>;
+            })}
+          </div>
+        </div>
+      </div>
+      <div id="divider"></div>
+      <div id="exp-title">Experience</div>
+      <div id="prevExp">
+        {Array.from(Object.values(props.data.experience.inputs)).map(
+          (e) => {
+            return (
+              <ExperiencePreview
+                company={e["company"]}
+                dates={e["dates"]}
+                location={e["location"]}
+                title={e["title"]}
+                tasks={e["tasks"]}
+              />
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Preview;
