e98743f3b1c50ee4085f85f0208bccbf79f0e33b
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 14:15:02 -0800

    Convert ExperiencePreview to function based component

--- src/components/ExperiencePreview.js
+++ src/components/ExperiencePreview.js
@@ -1,22 +1,19 @@
-import { Component } from "react";
 import "../styles/ExperiencePreview.css";
 
-class ExperiencePreview extends Component {
-  render() {
-    return (
-      <div className="prevExp-block">
-        <div className="top-row">
-            <div className="company-name">{this.props.company}</div>
-            <div className="job-length">{this.props.dates}</div>
-        </div>
-        <div className="job-title">{this.props.title}</div> 
-        <div className="job-location">{this.props.location}</div>
-        {(Array.from(Object.values(this.props.tasks))).map((e) => {
-            return <div className="jobtask-prev"><div className="task-dash">-</div>{e}</div>
-        })}
+const ExperiencePreview = (props) => {
+  return (
+    <div className="prevExp-block">
+      <div className="top-row">
+          <div className="company-name">{props.company}</div>
+          <div className="job-length">{props.dates}</div>
       </div>
-    );
-  }
+      <div className="job-title">{props.title}</div> 
+      <div className="job-location">{props.location}</div>
+      {(Array.from(Object.values(props.tasks))).map((e) => {
+          return <div className="jobtask-prev"><div className="task-dash">-</div>{e}</div>
+      })}
+    </div>
+  );
 }
 
 export default ExperiencePreview;
