c6c7935b6a1766cc612686782c05537bc6b50406
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 13:36:01 -0800

    Convert SkillsForm to function based component

--- src/components/SkillsForm.js
+++ src/components/SkillsForm.js
@@ -1,26 +1,19 @@
-import { Component } from "react";
-import '../styles/SkillsForm.css'
+import "../styles/SkillsForm.css";
 
-class SkillsForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <div id="skills-title-edit">Skills</div>
-        <form id="skills-form">
-          <textarea 
-            name="skills" 
-            onChange={this.props.handleChange} 
-            value={this.props.skills}
-            placeholder="Skill1,skill2,skill3, etc"
-          />
-        </form>
-      </>
-    );
-  }
-}
+const SkillsForm = (props) => {
+  return (
+    <>
+      <div id="skills-title-edit">Skills</div>
+      <form id="skills-form">
+        <textarea
+          name="skills"
+          onChange={props.handleChange}
+          value={props.skills}
+          placeholder="Skill1,skill2,skill3, etc"
+        />
+      </form>
+    </>
+  );
+};
 
 export default SkillsForm;
