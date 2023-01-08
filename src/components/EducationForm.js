1ac17790cf9ea896562dd88e0947d71b1ec0c832
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-08 14:25:16 -0800

    Convert EducationForm.js to function based component

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,44 +1,37 @@
-import { Component } from "react";
 import '../styles/EducationForm.css'
 
-class EducationForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <div id="edu-title-edit">Education</div>
-        <form id="education">
-          <input
-            id="school"
-            name="school"
-            type="text"
-            placeholder="Organization"
-            onChange={this.props.handleChange}
-            value={this.props.school}
-          />
-          <input
-            id="degree"
-            name="degree"
-            type="text"
-            placeholder="Degree/Major"
-            onChange={this.props.handleChange}
-            value={this.props.degree}
-          />
-          <input
-            id="startend"
-            name="duration"
-            type="text"
-            placeholder="Start year - End Year"
-            onChange={this.props.handleChange}
-            value={this.props.duration}
-          />
-        </form>
-      </>
-    );
-  }
+const EducationForm = (props) => {
+  return (
+    <>
+      <div id="edu-title-edit">Education</div>
+      <form id="education">
+        <input
+          id="school"
+          name="school"
+          type="text"
+          placeholder="Organization"
+          onChange={props.handleChange}
+          value={props.school}
+        />
+        <input
+          id="degree"
+          name="degree"
+          type="text"
+          placeholder="Degree/Major"
+          onChange={props.handleChange}
+          value={props.degree}
+        />
+        <input
+          id="startend"
+          name="duration"
+          type="text"
+          placeholder="Start year - End Year"
+          onChange={props.handleChange}
+          value={props.duration}
+        />
+      </form>
+    </>
+  );
 }
 
 export default EducationForm;
